e1e1368f60b5c24cc711b42025a15350f3e1465c
mikegsCoder <mikegsid@gmail.com>
2022-07-15 18:57:51 +0300

    Add App.js

---
+++ travel-destinations-react-app/src/App.js
@@ -0,0 +1,21 @@
+// import logo from './logo.svg';
+//import './App.css';
+
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+
+    return (
+        <div className="App">
+            
+            <main id="site-content">
+                <Routes>
+
+                </Routes>
+            </main>
+
+        </div>
+    );
+}
+
+export default App;
