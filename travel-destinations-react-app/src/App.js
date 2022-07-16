fcfae63988a51d594a91b2a670e7f26318e3d666
mikegsCoder <mikegsid@gmail.com>
2022-07-16 12:53:29 +0300

    Add Header component in App.js

--- travel-destinations-react-app/src/App.js
+++ travel-destinations-react-app/src/App.js
@@ -3,18 +3,20 @@
 
 import { Routes, Route } from 'react-router-dom';
 
+import Header from './components/Header';
 import HomePage from './components/HomePage';
 
 function App() {
 
     return (
         <div className="App">
+            <Header />
             
             <main id="site-content">
                 <Routes>
                     <Route path="/" element={<HomePage />} />
                     <Route path="/home-page" element={<HomePage />} />
-                    
+
                 </Routes>
             </main>
 
