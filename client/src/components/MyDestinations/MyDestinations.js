e9bd7bca6de9adbdd0b50c012117fe77b6e73ad6
mikegsCoder <mikegsid@gmail.com>
2022-07-26 18:14:49 +0300

    Add MyDestinations

---
+++ client/src/components/MyDestinations/MyDestinations.js
@@ -0,0 +1,46 @@
+import './MyDestinations.css';
+
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+import * as destinationService from '../../services/destinationService';
+import { useAuthContext } from '../../contexts/AuthContext';
+import DestinationCarousel from '../DestinationCarousel';
+import LoadingSpinner from '../Common/Spinner';
+import { useNotificationContext, types } from '../../contexts/NotificationContext';
+
+const MyDestinations = () => {
+    const { user } = useAuthContext();
+    const { addNotification } = useNotificationContext();
+    const [destinations, setDestinations] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        setIsLoading(true);
+
+        destinationService.getMyDestinations(user._id)
+            .then(destinationResult => {
+                setDestinations(destinationResult);
+                setIsLoading(false);
+            })
+            .catch(err => {
+                addNotification(err, types.danger);
+                setIsLoading(false);
+            });
+    }, []);
+
+    const myDestinations = (
+        <div className="my-destinatins-carousel" >
+            <h4>My Destinations: {destinations.length}</h4>
+            <DestinationCarousel destinations={destinations} />
+        </div>
+    );
+
+    return (
+        <>
+            {isLoading ? <LoadingSpinner /> : myDestinations}
+        </>
+    );
+}
+
+export default MyDestinations;
