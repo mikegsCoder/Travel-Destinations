27bdb21667d402f4a81f58006903cd25cc675cc4
mikegsCoder <mikegsid@gmail.com>
2022-07-30 12:28:23 +0300

    Fixed problem with reportWebVitals

--- client/src/services/destinationService.js
+++ client/src/services/destinationService.js
@@ -1,6 +1,6 @@
-const baseUrl = 'http://localhost:3030/data';
-
 import * as request from './requester';
+
+const baseUrl = 'http://localhost:3030/data';
 
 export const getAllDestinations = () => request.get(`${baseUrl}/destinations`);
 
