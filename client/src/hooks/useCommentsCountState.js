f7930c4acbc9ea4ab84a023fc849c5d7c266ebd7
mikegsCoder <mikegsid@gmail.com>
2022-08-01 17:41:36 +0300

    Add useCommentsCountState

---
+++ client/src/hooks/useCommentsCountState.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+import * as commentService from '../services/commentService';
+
+const useCommentState = (destinationId) => {
+    const [commentsCount, setCommentsCount] = useState(0);
+
+    useEffect(() => {
+        commentService.getCommentsCountBydestinationId(destinationId)
+        .then(result => {
+            setCommentsCount(result);
+        });
+    }, [destinationId]);
+
+    return [
+        commentsCount,
+        setCommentsCount
+    ];
+};
+
+export default useCommentState;
