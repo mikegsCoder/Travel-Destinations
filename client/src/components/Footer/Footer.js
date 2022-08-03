2e3f90e40d3ea2cbb2fcc0c144d5f2da07682fa0
mikegsCoder <mikegsid@gmail.com>
2022-08-03 17:40:37 +0300

    Refactored Footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -6,13 +6,13 @@ const Footer = () => {
         <div className="site-footer">
             <p className="footer-left">
                 Travel Destinations v_1.6 &copy; 2022 - {' '}
-                <a href="https://github.com/mikegsCoder/Travel-Destinations" target="_blank">
+                <a href="https://github.com/mikegsCoder/Travel-Destinations" target="_blank" rel="noreferrer">
                     mikegsCoder
                 </a>
             </p>
             <p className="footer-right">
                 React Course Project @ {' '}
-                <a href="https://softuni.bg/" target="_blank">
+                <a href="https://softuni.bg/" target="_blank" rel="noreferrer">
                     SoftUni
                 </a>
             </p>
